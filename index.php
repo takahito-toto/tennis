97d08a001d7e9b341d9aff23aca31ed4891f3dd6
takahito-toto <takahito.otomori@gmail.com>
2026-04-16 11:57:41 +0900

    9-2_index.php

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+<?php $info = file_get_contents("info.txt") ?>
 <!doctype html>
 <html lang="ja">
   <head>
@@ -10,7 +11,8 @@
     <main role="main" class="container" style="padding:60px 15px 0">
         <div>
 
-            <h1>サークルサイト</h1>
+            <h1>お知らせ</h1>
+            <p><?php echo $info; ?></p>
 
         </div>
     </main>
