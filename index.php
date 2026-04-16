36253d52e923be40ddf4b2384cf9e000fc2f48db
takahi-toto <takahito.otomori@gmail.com>
2026-04-16 10:29:16 +0900

    8-1_index.php

---
+++ index.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html lang="ja">
+  <head>
+    <title>サークルサイト</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css">
+  </head>
+  <body>
+
+    <?php include('navbar.php'); ?>
+    <main role="main" class="container" style="padding:60px 15px 0">
+        <div>
+
+            <h1>サークルサイト</h1>
+
+        </div>
+    </main>
+
+    <scriptsrc="https://code.jquery.com/jquery-3.5.1.slim.min.js" crossorigin="anonymous"></script>
+    <scriptsrc="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+    
+  </body>
+</html>
