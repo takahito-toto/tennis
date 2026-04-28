a5f12d1fb47fcd9781489eda71244fd188d56cea
takahito-toto <takahito.otomori@gmail.com>
2026-04-28 15:48:28 +0900

    10-6_album.php

--- navbar.php
+++ navbar.php
@@ -6,10 +6,9 @@
 
   <div class="collapse navbar-collapse" id="navbarsExampleDefult">
     <ul class="navbar-nav mr-auto">
-      <li class="nav-item"><a class="nav-link" href="info.php">
-      お知らせ</a></li>
-      <li class="nav-item"><a class="nav-link" href="#">アルバム</a>
-    </li>
+      <li class="nav-item"><a class="nav-link" href="info.php">お知らせ</a></li>
+      <li class="nav-item"><a class="nav-link" href="upload.php">画像アップロード</a></li>
+      <li class="nav-item"><a class="nav-link" href="album.php">アルバム</a></li>
       <li class="nav-item"><a class="nav-link" href="#">掲示板</a></li>
     </ul>
   </div>
