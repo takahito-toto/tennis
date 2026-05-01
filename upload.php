e95c2c2130084c10083e95337e8ba0d5ac044760
takahito-toto <takahito.otomori@gmail.com>
2026-05-01 13:43:02 +0900

    10-7_upload.php

--- upload.php
+++ upload.php
@@ -1,4 +1,20 @@
-<?php $fp = fopen("info.txt","r") ?>
+<?php 
+$msg = null;
+$alert = null;
+
+if(isset($_FILES['image']) && is_uploaded_file($_FILES['image']['tmp_name'])){
+  $old_name = $_FILES['image']['tmp_name'];
+  $new_name = $_FILES['image']['name'];
+  if(move_uploaded_file($old_name,'album/'.$new_name)){
+    $msg = 'アップロードしました。';
+    $alert = 'success';
+  } else {
+    $msg = 'アップロードできませんでした。';
+    $alert = 'danger';
+  }
+}
+?>
+
 <!doctype html>
 <html lang="ja">
   <head>
@@ -10,7 +26,12 @@
   <main role="main" class="container" style="padding:60px 15px 0">
 
   <h1>画像アップロード</h1>
-  <form action="uplod.php" method="post" enctype="multipart/form-data"> 
+  <?php
+  if($msg) {
+    echo '<div class = "alert alert-'.$alert.'" role="alret">'.$msg.'</div>';
+  }
+  ?>
+  <form action="upload.php" method="post" enctype="multipart/form-data"> 
     <div class="form-group">
       <label>アップロードファイル</label>
       <input type="file" name="image" class="form-control-file">
